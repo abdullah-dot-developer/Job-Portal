8df50a0d6dd4cd7823b58c3254837ea3f77c79c9
abdullah-dot-programmer <a22795101@gmail.com>
2024-08-17 15:33:55 +0500

    debugging phase 2

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
     try {
         console.log(req.cookies)
+        console.log(req.body)
         const token = req.cookies.token;
         console.log("token", token);
 
