626f34308a3ab99db076ba734a0e886bec5309af
abdullah-dot-programmer <a22795101@gmail.com>
2024-08-17 14:39:53 +0500

    token error solved

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -101,7 +101,7 @@ export const login = async (req, res) => {
             {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: "none",
+                sameSite: "lax",
                 secure: true
             }
         ).json({
